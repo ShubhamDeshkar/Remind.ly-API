a49d25746520ddcd13c27012e5257f87bfd0206e
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-08-08 11:08:17 -0400

    complete getTodo method

--- src/main/java/com/shubhamdeshkar/remindly/controller/TodoController.java
+++ src/main/java/com/shubhamdeshkar/remindly/controller/TodoController.java
@@ -3,11 +3,13 @@ package com.shubhamdeshkar.remindly.controller;
 import com.shubhamdeshkar.remindly.entity.Todo;
 import com.shubhamdeshkar.remindly.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = {"*"})
@@ -28,11 +30,14 @@ public class TodoController {
         return ResponseEntity.created(uri).build();
     }
 
-//    @GetMapping("/todos/{todoId}")
-//    public void getTodo(@PathVariable String uId, @PathVariable String todoId) {
-//
-//    }
-//
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity getTodo(@PathVariable String uId, @PathVariable String todoId) {
+        Optional<Todo> optionalTodo = todoRepo.findById(todoId);
+        if (optionalTodo.isPresent() && optionalTodo.get().getUser().getId().equals(uId))
+            return ResponseEntity.ok(optionalTodo.get());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
 //    @PutMapping("/todos/{todoId}")
 //    public void updateTodo(@PathVariable String uId, @PathVariable String todoId) {
 //
